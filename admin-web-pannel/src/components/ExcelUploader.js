fa11626c9db971c833841399551c67b27080351c
Modeiro Tegue <mikelmodeirotegue@gmail.com>
2025-05-29 13:01:46 +0100

    Admin Web Pannel, and Excel Parsing to Json

--- admin-web-pannel/src/components/ExcelUploader.js
+++ admin-web-pannel/src/components/ExcelUploader.js
@@ -21,7 +21,7 @@ const ExcelUploader = () => {
 
       try {
         await axios.post('http://localhost:4000/api/timetable', {
-          year,
+          year, 
           semester,
           timetable: json,
         });
